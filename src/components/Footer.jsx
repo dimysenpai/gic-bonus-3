4f0a4747bf35b5337af346e199e823d481920b50
dimysenpai <dimitrymabom@gmail.com>
2023-05-12 14:02:20 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/imgs/logo/GIC_BONUS_LOGO.png";
+import logoText from "../assets/imgs/logo/GIC_BONUS_TEXT.png";
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="links">
+        <Link to="/">Accueil</Link>
+        <Link to="/service">Services</Link>
+        <Link to="/training">Formations</Link>
+        <Link to="/about">A Propos</Link>
+      </div>
+      <div className="logoContent">
+        <img className="logo" src={logo} alt="logo" />
+        <img className="logoText" src={logoText} alt="logo" />
+      </div>
+      <div className="contact"></div>
+    </div>
+  );
 }
 
 export default Footer;
